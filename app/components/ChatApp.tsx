e5cbdccdf64286407e9ed1ea03e5d52564d35323
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-12-09 00:25:12 +0530

    Created the chat layout

--- app/components/ChatApp.tsx
+++ app/components/ChatApp.tsx
@@ -14,8 +14,6 @@ const ChatApp = () => {
     socket.on('usersList', (usersList) => {
       setUsersList(usersList);
     });
-
-   
   }, []);
 
   const handleLogin = (userName: string) => {
@@ -31,19 +29,33 @@ const ChatApp = () => {
         <>
           <div className="grid grid-cols-4 h-screen bg-slate-200">
             <div className="col-span-1 border-r-2 border-gray-400">
-              <h2 className='text-2xl p-2'>Connected Client</h2>
+              <h2 className="text-2xl p-2">Connected Client</h2>
               <div className=" flex flex-col">
                 {usersList.map((user) => {
                   return (
-                    <Link key={user} href={'/'} className="p-2 hover:bg-slate-300">
+                    <Link
+                      key={user}
+                      href={'/'}
+                      className="p-2 hover:bg-slate-300"
+                    >
                       {user}
                     </Link>
                   );
                 })}
               </div>
             </div>
-            <div className="col-span-3">
-              <h2>Public Chat</h2>
+            <div className="col-span-3 w-full">
+              <div className="flex flex-col-reverse h-screen">
+                <div className='flex'>
+                  <input className="input input-bordered mb-8 ml-4 mr-4 flex-grow" />
+                  <button className='btn btn-success mr-4'>Send</button>
+                </div>
+
+                <div className="bg-slate-300 ml-4 mb-2 flex-grow mr-4 overflow-auto">
+                  
+                </div>
+                <div className='bg-slate-400 ml-4 p-2 mr-4'>Public Message</div>
+              </div>
             </div>
           </div>
         </>
